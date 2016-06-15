7e6a7d90683f8f602e112d532ad02637efa5c658
dskwarek <dskwarek@motorolasolutions.com>
2016-06-15 10:14:22 +0200

    fix

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use('/users', users);
 app.get('/book/:isbn?', function(req, res, next) {
 
   var requestId = req.headers['x-request-id'] || Math.random();
-  res.estiOptions = {
+  res.esiOptions = {
     headers: {
       "x-request-id": requestId
     }
